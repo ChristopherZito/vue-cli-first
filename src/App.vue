9d95c820a25ee15d0b754be8ffcee046fc52bf73
christopherzito <christopherzito11@gmail.com>
2021-11-23 14:34:21 +0100

    update bonus bg img

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Hero/>
+    <div id="page">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <Title/>
+    </div>
+    
+    
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>     Pagina di base di vue-->
-    <Title msg="Welcome to my first app"/>
   </div>
 </template>
 
 <script>
 /* import HelloWorld from './components/HelloWorld.vue' */
 import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    /* HelloWorld, */
     Title,
+    Hero,
+    /* HelloWorld, */
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+#page {
+  position: relative;
+}
+
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="jumbo">
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
+
+<style scoped>
+
+.jumbo {
+    background-image: url("https://www.wallpapertip.com/wmimgs/2-21988_hd-computer-wallpaper-nature-hd-images-for-desktop.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    z-index: 0;
+}
+
+</style>
